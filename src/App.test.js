59ee4d8ab499ba6a4a938d996bad046450e597e6
shubhamwani96 <shubhamwani96@gmail.com>
2025-03-13 21:08:27 +0530

    Test case for search name

--- src/App.test.js
+++ src/App.test.js
@@ -1,7 +1,8 @@
-import { render, screen, fireEvent,waitFor  } from "@testing-library/react";
+import { render, screen, fireEvent, waitFor } from "@testing-library/react";
 import App from "./App";
 import { Button } from "./components/button/Button";
 import { RewardPoints } from "./composites/rewardPoints/RewardPoints";
+
 import userEvent from "@testing-library/user-event";
 
 test("Header renders with react testing from the appliction", () => {
@@ -46,7 +47,7 @@ test("Calculate Rewards button click triggers function", () => {
   expect(handleClick).toHaveBeenCalledTimes(1);
 });
 
-test("updates input value on change", async() => {
+test("updates input value on change", async () => {
   render(<RewardPoints />);
   const searchInput = screen.getByPlaceholderText("Search customers by name");
   fireEvent.change(searchInput, { target: { value: "test" } });
@@ -54,3 +55,52 @@ test("updates input value on change", async() => {
   await waitFor(() => expect(searchInput.value).toBe("test"));
 });
 
+test("filters customer data based on search input", async () => {
+  const mockRewardData = [
+    {
+      customerId: 1,
+      name: "Alice",
+      transactions: [
+        { date: "January 12, 2025", amount: 120 },
+        { date: "January 10, 2025", amount: 75 },
+        { date: "February 16, 2025", amount: 200 },
+        { date: "February 05, 2025", amount: 50 },
+        { date: "March 09, 2025", amount: 95 },
+      ],
+    },
+    {
+      customerId: 2,
+      name: "Bob",
+      transactions: [
+        { date: "January 21, 2025", amount: 45 },
+        { date: "January 23, 2025", amount: 150 },
+        { date: "February 14, 2025", amount: 85 },
+        { date: "February 19, 2025", amount: 130 },
+        { date: "March 10, 2025", amount: 55 },
+      ],
+    },
+  ];
+
+  render(<RewardPoints rewardData={mockRewardData} Loading={false} />);
+
+  const searchInput = screen.getByPlaceholderText("Search customers by name");
+  const rewardButon = screen.getByText("Calculate Rewards");
+
+  // Simulate clicking the 'Calculate Rewards' button
+  fireEvent.click(rewardButon);
+
+  // Wait for the component to re-render with filtered data
+  await waitFor(() => {
+    const rows = screen.getAllByRole("row");
+    // Assert that only two row is displayed (the header row plus the 'Alice' row)
+    expect(rows).toHaveLength(3);
+  });
+  // Simulate typing 'Alice' into the search input
+  fireEvent.change(searchInput, { target: { value: "Alice" } });
+
+  await waitFor(() => {
+    const rows = screen.getAllByRole("row");
+    // Optionally, assert that the row contains 'Alice'
+    expect(rows).toHaveLength(2);
+  });
+});
